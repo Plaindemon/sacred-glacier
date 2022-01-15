122c5314b6eaea5f7ca46b5edc34081669b48064
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-01-15 10:14:37 -0700

    added indexedDB

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -23,7 +23,21 @@ const commentController = {
       })
       .catch(err => res.json(err));
   },
-
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
   // remove comment
   removeComment({ params }, res) {
       Comment.findOneAndDelete({ _id: params.commentId })
@@ -46,23 +60,6 @@ const commentController = {
       })
       .catch(err => res.json(err));
 
-  },
-
-  
-  addReply({ params, body }, res) {
-    Comment.findOneAndUpdate(
-      { _id: params.commentId },
-      { $push: { replies: body } },
-      { new: true }
-    )
-      .then(dbPizzaData => {
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
-      .catch(err => res.json(err));
   },
 // remove reply
 removeReply({ params }, res) {
